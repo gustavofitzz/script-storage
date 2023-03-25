19377cf34df33774e46f228c9e72fd3edb17e85d
gustavofitzz <65750096+gustavofitzz@users.noreply.github.com>
2023-03-25 00:44:40 -0300

    Update Hat.lua

--- Hat.lua
+++ Hat.lua
@@ -76,34 +76,34 @@ rotx = 0;
 rotz = math.pi / 2;
 height = 0;
 heighti = 1;
-offset = 3;
-speed = 0.5;
+_G._G.offsett = 3;
+_G.speed = 0.5;
 mode = 9;
-offsett = 35;
-speedd = 20;
-val1 = 0;
-val2 = 0;
-val3 = 0;
-val4 = 0;
-val5 = 360;
+_G.offsettt  = 35;
+_G.speedd = 20;
+_G.val1 = 0;
+_G.val2 = 0;
+_G.val3 = 0;
+_G.val4 = 0;
+_G.val5 = 360;
 hihi = 0;
 angular = Vector3.new(0, 0, 0);
 local l = 1;
 game['Run Service'].RenderStepped:Connect(function()
 	--gamerpart
 	--gamerpart.CFrame = CFrame.new(chr.HumanoidRootPart.CFrame.Position.X, chr.HumanoidRootPart.CFrame.Position.Y + hihi, chr.HumanoidRootPart.CFrame.Position.Z)
-	--rotx = rotx + speed / 100;
-	--rotz = rotz + speed / 100;
-	l = (l >= 360 and 1 or l + speed);
+	--rotx = rotx + _G.speed / 100;
+	--rotz = rotz + _G.speed / 100;
+	l = (l >= 360 and 1 or l + _G.speed);
 	
 	for i, v in pairs(par) do
-		v.CFrame = CFrame.new(chr.HumanoidRootPart.Position) * CFrame.fromEulerAnglesXYZ(val1, math.rad(l + (val5 / #par) * i + speed), val2) * CFrame.new(offset, val3, val4);
+		v.CFrame = CFrame.new(chr.HumanoidRootPart.Position) * CFrame.fromEulerAnglesXYZ(_G.val1, math.rad(l + (_G.val5 / #par) * i + _G.speed), _G.val2) * CFrame.new(_G.offsett, _G.val3, _G.val4);
 	end
 	
 	if heighti == 1 then
-		height = height + speed / 100;
+		height = height + _G.speed / 100;
 	elseif heighti == 2 then
-		height = height - speed / 100;
+		height = height - _G.speed / 100;
 	end
 	if height > 2 then
 		heighti = 2;
@@ -114,15 +114,15 @@ game['Run Service'].RenderStepped:Connect(function()
 	
 	if mode == 1 then
 		for _, v in pairs(mov) do
-			v.Position = Vector3.new(math.sin(rotx) * offset, 0, math.sin(rotz) * offset);
+			v.Position = Vector3.new(math.sin(rotx) * _G.offsett, 0, math.sin(rotz) * _G.offsett);
 		end
 	elseif mode == 2 then
 		for _, v in pairs(mov) do
-			v.Position = Vector3.new(offset, height, offset);
+			v.Position = Vector3.new(_G.offsett, height, _G.offsett);
 		end
 	elseif mode == 3 then
 		for _, v in pairs(mov) do
-			v.Position = Vector3.new(math.sin(rotx) * offset, height, math.sin(rotz) * offset);
+			v.Position = Vector3.new(math.sin(rotx) * _G.offsett, height, math.sin(rotz) * _G.offsett);
 		end
 	elseif mode == 4 then
 		for i, v in pairs(mov) do
@@ -130,19 +130,19 @@ game['Run Service'].RenderStepped:Connect(function()
 		end
 	elseif mode == 5 then
 		for i, v in pairs(mov) do
-			v.Position = Vector3.new((math.sin(rotx)) * offset, height, (math.cos(rotz) - i) * offset);
+			v.Position = Vector3.new((math.sin(rotx)) * _G.offsett, height, (math.cos(rotz) - i) * _G.offsett);
 		end
 	elseif mode == 6 then
 		for i, v in pairs(mov) do
-			v.Position = Vector3.new((math.sin(rotx)) * offset, height, (math.tan(rotz) - i) * offset);
+			v.Position = Vector3.new((math.sin(rotx)) * _G.offsett, height, (math.tan(rotz) - i) * _G.offsett);
 		end
 	elseif mode == 7 then
 		for i, v in pairs(mov) do
-			v.Position = Vector3.new(math.cos(rotx * i) * offset, 0, math.cos(rotz * i) * offset);
+			v.Position = Vector3.new(math.cos(rotx * i) * _G.offsett, 0, math.cos(rotz * i) * _G.offsett);
 		end
 	elseif mode == 8 then
 	    for i, v in pairs(mov) do
-			v.Position = Vector3.new(math.sin(rotx) * i * offset, 0, math.sin(rotz) * i * offset);
+			v.Position = Vector3.new(math.sin(rotx) * i * _G.offsett, 0, math.sin(rotz) * i * _G.offsett);
 		end
 	elseif mode == 9 then
 		pcall(function()
@@ -158,8 +158,8 @@ game['Run Service'].RenderStepped:Connect(function()
 				end
 			end
 			if so ~= nil then
-				offset = so.PlaybackLoudness / offsett;
-				speed = so.PlaybackLoudness / speedd;
+				_G._G.offsett = so.PlaybackLoudness / _G._G.offsettt ;
+				_G.speed = so.PlaybackLoudness / _G.speedd;
 				angular = Vector3.new(so.PlaybackLoudness / ang, so.PlaybackLoudness / ang, so.PlaybackLoudness / ang);
 			end
 		end)
@@ -167,7 +167,7 @@ game['Run Service'].RenderStepped:Connect(function()
 			v.Position = Vector3.new(chr.HumanoidRootPart.CFrame:ToObjectSpace(CFrame.new(par[i].Position)).X, chr.HumanoidRootPart.CFrame:ToObjectSpace(CFrame.new(par[i].Position)).Y, chr.HumanoidRootPart.CFrame:ToObjectSpace(CFrame.new(par[i].Position)).Z);
 		end
 	elseif mode == 10 then
-		offset = height * 15;
+		_G.offsett = height * 15;
 		for i, v in pairs(mov) do
 			v.Position = Vector3.new(chr.HumanoidRootPart.CFrame:ToObjectSpace(CFrame.new(par[i].Position)).X, chr.HumanoidRootPart.CFrame:ToObjectSpace(CFrame.new(par[i].Position)).Y, chr.HumanoidRootPart.CFrame:ToObjectSpace(CFrame.new(par[i].Position)).Z);
 		end
@@ -187,14 +187,14 @@ game.Players.LocalPlayer.Chatted:Connect(function(c)
 			v.Parent = ftp(c:split(' ')[2])[1].Character.HumanoidRootPart;
 		end
 	end
-	if c:split(' ')[1] == '.speed' then
-		speed = tonumber(c:split(' ')[2]);
+	if c:split(' ')[1] == '._G.speed' then
+		_G.speed = tonumber(c:split(' ')[2]);
 	end
 	if c:split(' ')[1] == '.mode' then
 		mode = tonumber(c:split(' ')[2]);
 	end
-	if c:split(' ')[1] == '.offset' then
-		offset = tonumber(c:split(' ')[2]);
+	if c:split(' ')[1] == '._G.offset' then
+		_G.offset = tonumber(c:split(' ')[2]);
 	end
 	if c:split(' ')[1] == '.angular' then
 		angular = Vector3.new(tonumber(c:split(' ')[2]), tonumber(c:split(' ')[3]), tonumber(c:split(' ')[4]));
