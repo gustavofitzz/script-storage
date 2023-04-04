61ca88d394f4750c1a4d64e58ee962533a1d2be5
gustavofitzz <65750096+gustavofitzz@users.noreply.github.com>
2023-04-04 14:02:12 -0300

    Update handy f3x tool.lua

--- handy f3x tool.lua
+++ handy f3x tool.lua
@@ -1,7 +1,18 @@
 local f3x = {}
-
+local plr = game.LocalPlayer
+local bck = plr.Backpack
+local character = plr.Character
+local btools = character:FindFirstChild("Building Tools") or bck:FindFirstChild("Building Tools")
 function f3x:printtest(var1)
 print(var1)
+end
+
+function f3x:new(thing,x,y,z,parent)
+local arg1 = "CreatePart"
+local arg2 = thing
+local arg3 = CFrame.new(x,y,z)
+local arg4 = parent
+btools.SyncAPI.ServerEndpoint:InvokeServer(arg1, arg2, arg3, arg4)
 end
 
 return f3x
