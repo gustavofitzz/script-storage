eb8951568d9e6ee2d59e612c0c8333d0be2292c0
gustavofitzz <65750096+gustavofitzz@users.noreply.github.com>
2023-05-10 19:11:09 -0300

    Update blacklist.lua

--- blacklist.lua
+++ blacklist.lua
@@ -3,7 +3,7 @@
 --t5 global ban from some games
 blacklistTIER1 = {"1817633556"}
 blacklistTIER2 = {""}
-blacklistTIER3 = {""}
+blacklistTIER3 = {"4472031176"}
 blacklistTIER4 = {""}
 blacklistTIER5 = {"1994698149","4231931255"}
 return blacklistTIER5,blacklistTIER4,blacklistTIER3,blacklistTIER2,blacklistTIER1
