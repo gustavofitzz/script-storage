f814bece2acd89a0486a82d6686189c6261393df
gustavofitzz <65750096+gustavofitzz@users.noreply.github.com>
2023-05-10 19:00:14 -0300

    Update blacklist.lua

--- blacklist.lua
+++ blacklist.lua
@@ -1,4 +1,9 @@
---TODO stop using usernames and use user ids
+--TODO stop using usernames and use user ids DONE
+--t1 warning upon game joining
 --t5 global ban from some games
-blacklistTIER5 = {"HDaam123" , "TTS_VIBE","WASUPHOMIE_dogwaterr","isaacdude1309"}
-return blacklistTIER5
+blacklistTIER1 = {"1817633556"}
+blacklistTIER2 = {""}
+blacklistTIER3 = {""}
+blacklistTIER4 = {""}
+blacklistTIER5 = {"1994698149","4231931255"}
+return blacklistTIER5,blacklistTIER4,blacklistTIER3,blacklistTIER2,blacklistTIER1
