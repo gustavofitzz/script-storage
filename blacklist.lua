5cfc5050adb9de76633b7faa0581679e6853270f
gustavofitzz <65750096+gustavofitzz@users.noreply.github.com>
2023-05-10 14:58:24 -0300

    Update blacklist.lua

--- blacklist.lua
+++ blacklist.lua
@@ -1,2 +1,4 @@
-blacklist = {"HDaam123" , "TTS_VIBE","WASUPHOMIE_dogwaterr"}
-return blacklist
+--TODO stop using usernames and use user ids
+--t5 global ban from some games
+blacklistTIER5 = {"HDaam123" , "TTS_VIBE","WASUPHOMIE_dogwaterr","isaacdude1309"}
+return blacklistTIER5
